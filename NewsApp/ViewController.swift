253ab10d5484d38125106cd9e448dca4cacdccdc
sdiik <ahmadshiddiq0@gmail.com>
2025-04-22 10:31:00 +0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by ahmad shiddiq on 22/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by ahmad shiddiq on 22/04/25.
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
